a08fa0404ed0f18ecadcdc308fde4c6ab4437b58
Arbaaz Saber <arbaazfb26@gmail.com>
2025-10-22 02:52:37 +0530

    Adding github actions

--- backend/app.py
+++ backend/app.py
@@ -27,4 +27,9 @@ async def root():
 
 if __name__ =="__main__":
     import uvicorn
-    uvicorn.run("app:app")
+    import os
+    
+    host = os.getenv("HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", "8000"))
+    
+    uvicorn.run("app:app", host=host, port=port)
